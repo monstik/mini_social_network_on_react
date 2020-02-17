11da16966f37591112a735cf813b04f3c27b982a
monstrik <55989179+monstik@users.noreply.github.com>
2020-02-17 18:24:16 +0200

    Добавил map в MyPosts

--- src/components/Profile/MyPosts/MyPosts.jsx
+++ src/components/Profile/MyPosts/MyPosts.jsx
@@ -3,11 +3,18 @@ import Post from "./Post/Post";
 import Style from "./MyPosts.module.css"
 
 const MyPosts = () => {
-    return(
+
+    let posts = [
+        {src: "https://s1.1zoom.ru/b5050/460/Tanks_441653_3840x2400.jpg"},
+        {src: "https://im0-tub-ua.yandex.net/i?id=8b5cd3e1b249a8fb5603cd8e63db9165&n=13"},
+        {src: "https://pbs.twimg.com/media/EFJA1rPWwAARCn4.jpg"},
+    ];
+
+    let postElements = posts.map(p => <Post img={p.src}/>);
+
+    return (
         <div className={Style.posts}>
-            <Post img={"https://s1.1zoom.ru/b5050/460/Tanks_441653_3840x2400.jpg"}/>
-            <Post img={"https://im0-tub-ua.yandex.net/i?id=8b5cd3e1b249a8fb5603cd8e63db9165&n=13"}/>
-            <Post img={"https://pbs.twimg.com/media/EFJA1rPWwAARCn4.jpg"}/>
+            {postElements}
         </div>
     );
 }
