3f6d65fefd2354934de02bedcb9492a3665e7d32
monstrik <55989179+monstik@users.noreply.github.com>
2020-02-17 18:16:35 +0200

    Использовал метод масива map для оптимизации и сокращения кода

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,21 +1,55 @@
 import React from "react";
 import Style from "./Dialogs.module.css"
+import {NavLink} from "react-router-dom";
+
+
+
+const DialogItem = (props) => {
+
+    let path = "/dialogs/" + props.id;
+
+    return(
+        <div className={Style.dialog}>
+            <NavLink to={path}>{props.name}</NavLink>
+        </div>
+    );
+};
+
+const Message = (props) => {
+
+    return(
+        <div className={Style.message}>{props.message}</div>
+    );
+};
 
 const Dialogs = (props) => {
+
+    let dialogs = [
+        {id:1, name: "Dania"},
+        {id:2, name: "Ilona"},
+        {id:3, name: "Katya"},
+        {id:4, name: "Nastya"},
+        {id:5, name: "Diana"},
+        {id:6, name: "Mask"},
+    ];
+
+    let messages = [
+        {message: "Hello"},
+        {message: "Hi"},
+        {message: "How are you?"},
+        {message: "I dont know"},
+    ];
+
+    let dialogsElements = dialogs.map(d => <DialogItem id={d.id} name={d.name}/>);
+    let messagesElements = messages.map(m => <Message message={m.message}/>);
+
   return(
       <div className={Style.dialogs}>
         <div className={Style.dialogsItem}>
-            <div className={Style.dialog + ' ' + Style.active}>Dania</div>
-            <div className={Style.dialog}>Andrey</div>
-            <div className={Style.dialog}>Sveta</div>
-            <div className={Style.dialog}>Sasha</div>
-            <div className={Style.dialog}>Victor</div>
+            {dialogsElements}
         </div>
           <div className={Style.messages}>
-              <div className={Style.message}>Hi</div>
-              <div className={Style.message}>Привет</div>
-              <div className={Style.message}>Как дела?</div>
-
+              {messagesElements}
           </div>
 
       </div>
